2a8e6f209675eb7d1d21822f40a92410a3bcf97d
Mingqiang Sun <davidmusksun1993@gmail.com>
2020-05-26 18:28:27 +0800

    [systemd] complete ssh-tunnel timer
    NOTICE:
    1. do not link service to /etc/systemd/system/
    2. (systemd.kill) default KillMode is control-group
    
    Signed-off-by: Mingqiang Sun <davidmusksun1993@gmail.com>

--- service/systemd/ssh-tunnel.sh
+++ service/systemd/ssh-tunnel.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -ex
+
 __pwd()
 {
   (cd `dirname $0` && pwd)
@@ -16,11 +18,12 @@ After=network.target time-sync.target
 Type=simple
 User=$2
 ExecStart=$3 check-remote-port
+KillMode=process
 
 [Install]
 WantedBy=multi-user.target
 EOF
-(cd $SYSTEMD_DIR && sudo ln -sfn $__PWD/$1)
+sudo mv $1 $SYSTEMD_DIR
 }
 
 CreateTimer()
@@ -30,14 +33,16 @@ CreateTimer()
 Description=Check Remote Port
 
 [Timer]
-OnCalendar=${2}min
+OnBootSec=1min
+OnUnitActiveSec=${2}min
 RandomizedDelaySec=5min
 Persistent=true
 
 [Install]
-WantedBy=timers.target
+WantedBy=multi-user.target
 EOF
-(cd $SYSTEMD_DIR && sudo ln -sfn $__PWD/$1)
+sudo mv $1 $SYSTEMD_DIR
+#(test -f $SYSTEMD_DIR/$1 || { cd $SYSTEMD_DIR; sudo ln -sfn $__PWD/$1; })
 }
 
 OnInit()
@@ -46,6 +51,42 @@ OnInit()
   __PWD=`__pwd`
   SELF=`basename $0`
   NAME=${SELF%.*}
+  REMOTE_HOST=rn
+  REMOTE_PORT=7788
+  LOCAL_PORT=`RetrieveLocalPort`
+  [ $LOCAL_PORT ] || LOCAL_PORT=22
+  PID_FILE=/tmp/$NAME.pid
+  LOG_FILE=/tmp/$NAME.log
+  chmod +x $0
+}
+
+KillProcess()
+{
+  local pid=$1
+  test -f $1 && pid=`cat $1`
+  [ $pid ] || return 1
+  kill -0 $pid &>/dev/null && kill -9 $pid
+}
+
+CheckRemotePort()
+{
+  ssh $1 "nc -zv localhost $2"
+}
+
+RetrieveLocalPort()
+{
+  sed -E -n 's/^[#]?Port[[:blank:]]+([0-9]+)/\1/p' ${1:-/etc/ssh/sshd_config}
+}
+
+StartTunnel()
+{
+  nohup ssh -v -NCR $2:localhost:$3 $1 &>$LOG_FILE &
+  echo $! > $PID_FILE
+}
+
+CheckTunnelProcess()
+{
+  ps aux | grep -v grep | grep -q $1
 }
 
 main()
@@ -57,7 +98,12 @@ main()
       CreateTimer ${2:-$NAME}.timer 1
       ;;
     1|check-remote-port)
-      echo `date` >>/tmp/trivial@test;;
+      if CheckTunnelProcess $REMOTE_PORT; then
+        CheckRemotePort $REMOTE_HOST $REMOTE_PORT || { KillProcess $PID_FILE; rm -f $PID_FILE; }
+      else
+        StartTunnel $REMOTE_HOST $REMOTE_PORT $LOCAL_PORT
+      fi
+      ;;
   esac
 }
 main $*
