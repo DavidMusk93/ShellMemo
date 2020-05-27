5a85fe152006c90490dd2a6b24f96f9729df08d7
Mingqiang Sun <davidmusksun1993@gmail.com>
2020-05-27 17:26:04 +0800

    [init] add utility.sh
    Signed-off-by: Mingqiang Sun <davidmusksun1993@gmail.com>

---
+++ init/utility.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+TrySource()
+{
+  test -f $1 && . $1
+}
+export TrySource
+
+SetProxy()
+{
+  case ${1:-'enable'} in
+    0|enable)
+      case ${2:-'v2ray'} in
+        v2ray)  export http_proxy=localhost:8123;;
+        trojan) export http_proxy=localhost:2091;;
+        *)      return 1;;
+      esac
+      export https_proxy=$http_proxy
+      ;;
+    1|disable)
+      export -n http_proxy
+      export -n https_proxy
+      ;;
+  esac
+}
