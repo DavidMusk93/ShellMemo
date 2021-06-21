766b2f3a906e9ff877d897585f1b0b3fdd950067
Mingqiang Sun <davidmusksun1993@gmail.com>
2021-06-21 18:25:34 +0800

    refactor on docker/openvpn.sh
    Signed-off-by: Mingqiang Sun <davidmusksun1993@gmail.com>

---
+++ docker/compromise.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+_build_color() {
+    echo -n '\e['"$1"'m'
+}
+
+initialize_color() {
+    COLORRED=$(_build_color 31)
+    COLORRESET=$(_build_color 0)
+}
+
+logfatal() {
+    initialize_color
+    echo -e "$COLORRED$*$COLORRESET"
+    exit 255
+}
+
+_yes() {
+    [ "$1" ] || return 1
+    [ "$1" -eq 0 ] && return 1
+    case $1 in
+    "FALSE" | "False" | "false") return 1 ;;
+    esac
+    return 0
+}
+
+# If a val is sensitive, load it form env may be a better choice.
+#  arguments:
+#   $1, val name set by 'export key=val'
+#   $2, optional/required
+loadvalfromenv() {
+    eval "${1,,}=\$$1"
+    _yes "$2" && ! [ "${!1}" ] && logfatal \""$1"\" is not set
+}
---
+++ docker/openvpn.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+const() {
+    SPACE='[[:blank:]]'
+    KVSEPARATOR='='
+    UNCOMMENTSIGN='#'
+    SEDMODIFYFLAG='-i'
+}
+
+initialize() {
+    const
+    datadir=$(whereami)/data
+    image=kylemanna/openvpn
+    host=tx2.guohuasun.com
+}
+
+whereami() {
+    dirname "$(realpath $0)"
+}
+
+cid() {
+    docker ps | grep "$1" | awk 'print $1'
+}
+
+setval() {
+    sed $SEDMODIFYFLAG -r 's/^('"$SPACE*$2$SPACE"'*'$KVSEPARATOR"$SPACE"'*).*/\1'"$3"'/' "$1"
+}
+
+uncomment() {
+    sed $SEDMODIFYFLAG -r 's/^['$UNCOMMENTSIGN']?'"$SPACE"'*('$2'.*)/\1/' "$1"
+}
+
+enable_forward() {
+    uncomment "$@"
+    setval "$@" 1
+}
+
+enable_service() {
+    local f targetdir
+    f=$(basename $1)
+    targetdir=/etc/systemd/system
+    test -f $targetdir/$f || {
+        cp $1 $targetdir
+        systemctl enable $f
+    }
+}
+
+_run() {
+    docker run -v "$datadir":/etc/openvpn "$@"
+}
+__run() {
+    _run --log-driver=none --rm "$@"
+}
+
+main() {
+    set -x
+    initialize
+    case $1 in
+    0 | start) _run -p 1194:1194/udp --cap-add=NET_ADMIN $image ;;
+    1 | stop) docker stop "$(cid $image)" ;;
+    2 | new)
+        u="${2:-default}"
+        __run -it $image easyrsa build-client-full "$u" nopass
+        __run $image ovpn_getclient "$u" >"$u".ovpn
+        ;;
+    3 | bootstrap)
+        __run $image ovpn_genconfig -u udp://$host -c -d -D -z -s "10.10.10.0/24"
+        __run -it $image ovpn_initpki
+        enable_forward /etc/sysctl.conf net.ipv4.ip_forward
+        ;;
+    4 | service)
+        this=$(realpath $0)
+        target=$(dirname $this)/openvpn.service
+        cat >$target <<eof
+[Unit]
+Description = Docker Openvpn
+After = network.target docker.service
+Requires = docker.service
+
+[Service]
+Type = simple
+User = ubuntu
+
+ExecStart = $this start
+ExecStop = $this stop
+
+[Install]
+WantedBy = multi-user.target
+eof
+        enable_service $target
+        ;;
+    esac
+}
+main "$@"
